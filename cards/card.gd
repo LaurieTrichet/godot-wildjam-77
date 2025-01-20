219fb44813de6d668e785e44bcb0b87315845960
Laurie Trichet <laurie.trichet@protonmail.com>
2025-01-20 14:11:48 +1100

    organise projects

---
+++ cards/card.gd
@@ -0,0 +1,5 @@
+extends Node
+
+signal updated(card_data: CardData)
+
+var card_data: CardData
---
+++ cards/card_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name CardData
+
+var title: String
+var description: String
+var next_stage_description: String
+var image: Texture2D
