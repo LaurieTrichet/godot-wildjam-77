2c08f8986c14a4b5c6c0bcfa8ea2b92ef6501055
Laurie Trichet <laurie.trichet@protonmail.com>
2025-01-31 17:42:07 +1100

    update main scene with the new prefabs

--- play_area/tutorial_1.gd
+++ play_area/tutorial_1.gd
@@ -4,10 +4,10 @@ extends Node
 @onready var open_pack_panel_container: PanelContainer = %OpenPackPanelContainer
 @onready var play_area_container: VBoxContainer = %PlayAreaContainer
 
-@onready var hand_container: HBoxContainer = %HandContainer
 @onready var pack_container: HBoxContainer = %PackContainer
 
-
+@export var hand_container: HBoxContainer 
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	play_area_container.visible = false
